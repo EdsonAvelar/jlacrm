33b7fa975944aab7f416bb2053bbbefd41490890
Adriano Avelar <eam.avelar@gmail.com>
2023-04-03 09:57:42 -0300

    1.3.3

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,6 +4,16 @@
 <html lang="en">
 
 <head>
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-T8M2RFC');</script>
+    <!-- End Google Tag Manager -->
+
+    
     <meta charset="utf-8">
     <title>JLA Soluções Financeiras</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -23,12 +33,17 @@
 
     @yield('headers')
 
+
+
 </head>
 
 <body class="loading"
     data-layout-config='{"leftSideBarTheme":"dark","layoutBoxed":false, "leftSidebarCondensed":false, "leftSidebarScrollable":false,"darkMode":false, "showRightSidebarOnStart": true}'>
     <!-- Begin page -->
-
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T8M2RFC"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 
     <div hidden="true" name="{{ url('/') }}" id="public_path"></div>
     <div class="wrapper">
