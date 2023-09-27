5afe579f8a5329a526b679194cf779c7a04d8cac
Adriano Avelar <eam.avelar@gmail.com>
2023-09-27 18:43:58 -0300

    correcao fechamento

--- database/migrations/2023_09_25_232755_add_campos_to_levantamento.php
+++ database/migrations/2023_09_25_232755_add_campos_to_levantamento.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('levantamentos', function (Blueprint $table) {
-            $table->string('planejamento');
+            $table->string('planejamento')->nullable();;
         });
     }
 
