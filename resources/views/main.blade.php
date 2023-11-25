11645e24261f78fe0ad03eb163430f26a603f2a7
Adriano Avelar <eam.avelar@gmail.com>
2023-11-25 10:23:59 -0300

    facebook

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,7 +8,10 @@ use Carbon\Carbon;
 <head>
 
     <meta charset="utf-8">
+    
+
     <meta name="facebook-domain-verification" content="2v42l4jph9cgfkrorr6gejjod4bqx6" />
+
     <title>{{ config('nome') }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description">
