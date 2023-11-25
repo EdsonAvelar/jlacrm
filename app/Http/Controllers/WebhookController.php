496fd2e2996389f02c7153ef24e6e8f48072cf46
Adriano Avelar <eam.avelar@gmail.com>
2023-11-24 21:08:41 -0300

    este webhook

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -7,7 +7,18 @@ use Illuminate\Http\Request;
 class WebhookController extends Controller
 {
     public function index(Request $request) {
-        
 
+        
+        $mode = $request->query('hub_mode');
+        $challenger = $request->query('hub_challenge');
+        $verify_token = $request->query('hub_verify_token');
+
+        $data = [ 
+            "hub.mode" => $mode,
+            "hub.challenge" => $challenger,
+            "hub.verify_token" => "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+        ];
+            
+        return json_encode($data); 
     }
 }
