91b70d836debd2113a5b748af088f6b50f5d60c3
Adriano Avelar <eam.avelar@gmail.com>
2024-09-18 17:29:44 -0300

    bateu a meta

--- resources/views/dashboards/ranking/templates/default_css.blade.php
+++ resources/views/dashboards/ranking/templates/default_css.blade.php
@@ -999,7 +999,7 @@
 
     .vendas-meta-batida {
         background-color: #2f6b34;
-        background-image: url("{{url('')}}/images/sistema/fera2.png");
+        background-image: url("{{url('')}}/images/sistema/fera.png");
         background-size: cover;
     }
 
